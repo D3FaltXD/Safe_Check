eb73151fc37fa4d4d00d5d2ec9bf9a477c1191b6
D3FaltXD <kalistoroy619@gmail.com>
2023-04-11 04:12:37 +0530

    Added removable option

--- js/app.js
+++ js/app.js
@@ -21,3 +21,11 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
     }
   });
 });
+function myFunction() {
+  var x = document.getElementById("hello");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
