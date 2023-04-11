afa4bfb9dac7ba3fd652f67c37286755a15e9379
D3FaltXD <kalistoroy619@gmail.com>
2023-04-11 06:02:06 +0530

    Pushing hard

--- js/app.js
+++ js/app.js
@@ -22,10 +22,15 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
   });
 });
 function myFunction() {
-  var x = document.getElementById("hello");
+  var x = document.getElementById("1");
+  var y= document.getElementById("2");
   if (x.style.display === "none") {
     x.style.display = "block";
+    y.style.display = "none";
+
   } else {
     x.style.display = "none";
+    y.style.display = "flex";
+
   }
 }
